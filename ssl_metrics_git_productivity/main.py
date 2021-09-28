e8c8ea40184df6e02e320bf1edb87a558adb1a2f
Matthew Hyatt <mhyatt@luc.edu>
2021-09-27 19:01:30 -0500

    overview in main

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -1,1 +1,7 @@
+import numpy as np
 
+# Import JSON file
+# TeamEffort = Last commit day_since_0
+# ModuleSize = loc_sum
+# Prod = moduleSize / Team Effort
+# Store all productivity information in some array or dataframe
