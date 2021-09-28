e5a8b88deeea186e9a93d1611f5e92c9386abf74
Matthew Hyatt <mhyatt@luc.edu>
2021-09-28 17:50:32 -0500

    calculated prod

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -1,7 +1,45 @@
+import json
+from pprint import pprint as print
+
 import numpy as np
 
-# Import JSON file
-# TeamEffort = Last commit day_since_0
-# ModuleSize = loc_sum
-# Prod = moduleSize / Team Effort
-# Store all productivity information in some array or dataframe
+
+def get_json(filename: str = "issues.json") -> list:
+    with open(file=filename, mode="r") as file:
+        return json.load(file)
+
+
+def team_effort(data) -> int:
+    return data[len(data) - 1]["day_since_0"]
+
+
+def module_size(data) -> list:
+    return [item.get("loc_sum") for item in data]
+
+
+def productivity(MS: list, TE: int) -> list:
+    return [float(loc / TE) for loc in MS]
+
+
+def main():
+    data = get_json(
+        "/Users/matthewhyatt/file-storage/loyola/s3/ssl/sohini-productivity/loc.json"
+    )
+
+    print(productivity(module_size(data), team_effort(data)))
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+convert to dict
+    with key as day
+        average prod for day
+        or by commit?
+    prod as value
+    store in json
+
+    pandas dataframe
+        df[df[days]==0]
+"""
