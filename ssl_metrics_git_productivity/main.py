508bf04aa01a6726ed9ba498311db210dc3331f2
Sohini Thota <sthota@luc.edu>
2021-10-03 11:35:05 -0500

    args commit

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -5,7 +5,7 @@ def getArgs():
     argumentParser = argparse.ArgumentParser(prog="SSL Metrics Git Productivity", usage= "Calculates productivity measure of a git project.")
     argumentParser.add_argument('--jsonfile', '-j', required = True, type = open, help = "Commit since day 0")
 
-# Import JSON file
+# Import JSON file using Argparse
 # TeamEffort = Last commit day_since_0
 # ModuleSize = loc_sum
 # Prod = moduleSize / Team Effort
