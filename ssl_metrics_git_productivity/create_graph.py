1078316b675522904a664a01a739fa10ae83e80f
Matthew Hyatt <mhyatt@luc.edu>
2021-10-04 18:39:37 -0500

    modified git-commits-loc/create_graph.py to get prod/create_graph.py

--- ssl_metrics_git_productivity/create_graph.py
+++ ssl_metrics_git_productivity/create_graph.py
@@ -1,26 +1,69 @@
 from argparse import ArgumentParser, Namespace
+from os import path
+
+import matplotlib.pyplot as plt
+import pandas
+from matplotlib.figure import Figure
+from pandas import DataFrame
+
 
 def get_argparse() -> Namespace:
     parser: ArgumentParser = ArgumentParser(
-        prog="PROGRAM NAME",
-        usage="PROGRAM DESCRIPTION",
+        prog="Convert Output",
+        usage="This program converts a JSON file into various different formats.",
     )
     parser.add_argument(
         "-i",
         "--input",
-        help="The input JSON file that is to be used for graphing",
+        help="The input data file that will be read to create the graphs",
         type=str,
         required=True,
     )
-
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="The filename to output the graph to",
+        type=str,
+        required=True,
+    )
     return parser.parse_args()
-  
-  def main() -> None:
-    args: Namespace = get_argparse()
+
+
+# prod_sum over time where time is spaced by day
+def plot(df: DataFrame, filename: str) -> None:
+    figure: Figure = plt.figure()
+    plt.ylabel("prod")
+    plt.xlabel("day_since_0")
+    plt.title("Daily Productivity Sum Over Time")
+
+    unique_days = list(set(df['day_since_0']))
+    print(unique_days)
+    prod_sum = [0 for day in unique_days]
+
+    for day in unique_days:
+        for i in range(len(df)):
+            if df.iloc[i]['day_since_0'] == day:
+                prod_sum[unique_days.index(day)] += abs(df.iloc[i]['productivity'])
+
+    print(prod_sum)
+
+    plt.bar(unique_days, prod_sum)
+    # plt.scatter(df['day_since_0'], df['productivity'])
+    figure.savefig(filename)
+
+
+def main():
+    args:Namespace = get_argparse()
 
     if args.input[-5::] != ".json":
         print("Invalid input file type. Input file must be JSON")
         quit(1)
+
+    df:DataFrame = pandas.read_json(args.input)
+    print(df)
+
+    plot(df, filename=args.output)
+
 
 if __name__ == "__main__":
     main()
